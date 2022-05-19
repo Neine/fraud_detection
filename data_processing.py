05baa0ecfc528793e9191e65c1f901487a8e3c4e
neine <neine112arora@gmail.com>
2022-05-19 18:21:43 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,23 @@
 
+from base64 import b16decode
+
+
 print ("adding all the data processing code related to fraud detection")
+
+print ("Hello World")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print("added a new function sum")
+
+
+
+
+
+
 
